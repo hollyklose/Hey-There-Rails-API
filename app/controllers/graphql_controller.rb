992dee37b2a8ff671b8ff602569841b94f742870
hollyklose <hollyklose@gmail.com>
2019-07-21 14:21:19 -0400

    Change GraphqlController to ProtectedController

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,11 +1,11 @@
-class GraphqlController < ApplicationController
+class GraphqlController < ProtectedController
   def execute
     variables = ensure_hash(params[:variables])
     query = params[:query]
     operation_name = params[:operationName]
     context = {
       # Query context goes here, for example:
-      # current_user: current_user,
+      # current_user: current_user
     }
     result = HeyThereRailsApiSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
