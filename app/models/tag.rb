772f6428d45564fda553131f9c2518c6b26a068c
hollyklose <hollyklose@gmail.com>
2019-09-06 17:46:33 -0400

    Make tag name not unique
    Removed unique requirement from tag name model to allow different
    users to have the same tags.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,8 +2,6 @@ class Tag < ApplicationRecord
   belongs_to :user
   has_many :tag_instances, dependent: :destroy
 
-  validates :name, uniqueness: true
-
   enum genre: {
     location: 'location',
     company: 'company',
